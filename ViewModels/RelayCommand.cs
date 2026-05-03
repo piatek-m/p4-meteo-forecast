fcc8febcff335ec1718a9e2e05a0b5a23542ceac
Marcin <piatekmarcin@proton.me>
2026-05-03 21:58:09 +0200

    Create RelayCommand implementing ICommand

---
+++ ViewModels/RelayCommand.cs
@@ -0,0 +1,27 @@
+using System.Windows.Input;
+
+namespace MeteoForecast.ViewModels;
+
+public class RelayComand : ICommand
+{
+    private readonly Action _execute;
+    private readonly Func<bool> _canExecute;
+
+    public RelayComand(Action execute, Func<bool> canExecute)
+    {
+        _execute = execute;
+        _canExecute = canExecute;
+    }
+
+    public event EventHandler? CanExecuteChanged;
+
+    // Notify subscribers when CanExecute has changed
+    public void RaiseCanExecuteChanged()
+        => CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+
+    public bool CanExecute(object? parameter)
+        => _canExecute?.Invoke() ?? true;
+
+    public void Execute(object? parameter)
+        => _execute();
+}
