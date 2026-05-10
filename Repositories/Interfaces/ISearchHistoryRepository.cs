ecc275d58c56d8d27764dd9656ba1755f3c086d3
Marcin <piatekmarcin@proton.me>
2026-05-10 20:16:56 +0200

    Fix ISearchHistoryRepository (from binary to text)

--- Repositories/Interfaces/ISearchHistoryRepository.cs
+++ Repositories/Interfaces/ISearchHistoryRepository.cs
@@ -1,1 +1,9 @@
-ÿþ
+using MeteoForecast.Models;
+
+namespace MeteoForecast.Repositories.Interfaces;
+
+public interface ISearchHistoryRepository : IRepository<SearchHistory>
+{
+    Task<List<SearchHistory>> GetRecentAsync(int count);
+    Task ClearAsync();
+}
