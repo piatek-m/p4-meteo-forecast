aed6ceff3861a30220a1ca185b3063473a30e41c
Marcin <piatekmarcin@proton.me>
2026-06-04 16:47:16 +0200

    Create async version of relayCommand

---
+++ ViewModels/Generics/AsyncRelayCommand.cs
@@ -0,0 +1,44 @@
+using System.Windows.Input;
+
+namespace MeteoForecast.ViewModels.Generics;
+
+public class AsyncRelayCommand : ICommand
+{
+    private readonly Func<Task> _execute;
+    private readonly Func<bool> _canExecute;
+    private bool _isExecuting;
+
+    public AsyncRelayCommand(Func<Task> execute, Func<bool> canExecute)
+    {
+        _execute = execute;
+        _canExecute = canExecute;
+    }
+
+    public event EventHandler? CanExecuteChanged;
+
+    // Notify subscribers when CanExecute has changed
+    public void RaiseCanExecuteChanged()
+        => CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+
+    public bool CanExecute(object? parameter)
+        => !_isExecuting && (_canExecute?.Invoke() ?? true);
+
+    public async void Execute(object? parameter)
+    {
+        if (_isExecuting)
+            return;
+
+        try
+        {
+            _isExecuting = true;
+            RaiseCanExecuteChanged();
+
+            await _execute();
+        }
+        finally
+        {
+            _isExecuting = false;
+            RaiseCanExecuteChanged();
+        }
+    }
+}
