0656bf7336884b0b2d855f8e4b34082a5b77ee68
Marcin <piatekmarcin@proton.me>
2026-06-05 01:29:35 +0200

    Fix types

---
+++ ViewModels/CityWeatherViewModel.cs
@@ -0,0 +1,65 @@
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+using MeteoForecast.Models;
+using MeteoForecast.Services.Interfaces;
+using MeteoForecast.ViewModels.Generics;
+namespace MeteoForecast.ViewModels;
+
+public class CityWeatherViewModel : BaseViewModel
+{
+    private readonly IWeatherCacheService _weatherCacheService;
+
+    private City? _selectedCity;
+    public City? SelectedCity
+    {
+        get => _selectedCity;
+        set => SetProperty(ref _selectedCity, value);
+    }
+
+    private DateTime _selectedDay = DateTime.Today;
+    public DateTime SelectedDay
+    {
+        get => _selectedDay;
+        set => SetProperty(ref _selectedDay, value);
+    }
+    public ObservableCollection<HourlyWeather> Forecast { get; } = [];
+
+    public ICommand NextDayCommand { get; }
+    public ICommand PreviousDayCommand { get; }
+
+    public CityWeatherViewModel(IWeatherCacheService weatherCacheService)
+    {
+        _weatherCacheService = weatherCacheService;
+
+        NextDayCommand = new AsyncRelayCommand(async _ => await NextDayAsync());
+        PreviousDayCommand = new AsyncRelayCommand(async _ => await PreviousDayAsync());
+    }
+
+    private async Task LoadForecastAsync()
+    {
+        if (SelectedCity is null)
+            return;
+
+        var data = await _weatherCacheService.GetWeatherForDayAsync(
+            SelectedCity.Id,
+            SelectedCity.Latitude,
+            SelectedCity.Longitude,
+            SelectedDay
+        );
+        Forecast.Clear();
+        foreach (var item in data)
+            Forecast.Add(item);
+    }
+
+    private async Task NextDayAsync()
+    {
+        SelectedDay = SelectedDay.AddDays(1);
+        await LoadForecastAsync();
+    }
+    private async Task PreviousDayAsync()
+    {
+        SelectedDay = SelectedDay.AddDays(-1);
+        await LoadForecastAsync();
+    }
+
+}
