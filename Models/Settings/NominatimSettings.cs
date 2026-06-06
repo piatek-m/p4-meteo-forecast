93325c5e087aa1dfbe1c303a97c1fea10053bda5
Marcin <piatekmarcin@proton.me>
2026-06-06 06:19:30 +0200

    Fix nominatim userAgent string

--- Models/Settings/NominatimSettings.cs
+++ Models/Settings/NominatimSettings.cs
@@ -2,5 +2,5 @@ namespace MeteoForecast.Models.Settings;
 
 public class NominatimSettings
 {
-    public string UserAgent { get; set; } = "MeteoForecast - A student project available at github.com/piatek-m/p4-meteo-forecast";
+    public string UserAgent { get; set; } = "MeteoForecast/1.0 (https://github.com/piatek-m/p4-meteo-forecast)";
 }
