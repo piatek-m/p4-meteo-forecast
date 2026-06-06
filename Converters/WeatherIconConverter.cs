df080c171a061084daeff86913c519115aaac9e2
Marcin <piatekmarcin@proton.me>
2026-06-06 05:50:33 +0200

    Weather icon converter takes int instead of string

--- Converters/WeatherIconConverter.cs
+++ Converters/WeatherIconConverter.cs
@@ -12,22 +12,21 @@ public class WeatherIconConverter : IValueConverter
 
         return code switch
         {
-            0 => "☀",           // Sunny
-            1 => "🌤",           // Mostly sunny
-            2 => "⛅",           // Partial overcast
-            3 => "☁",        // Overcast
-            45 or 48 => "🌫",   // Fog
-            51 or 53 or 55 => "🌦",  // Light rain
-            61 or 63 or 65 => "🌧",  // Rain
-            71 or 73 or 75 or 77 => "❄",  // Snow
-            80 or 81 or 82 => "☔",  // Showers (przelotne opady)
-            85 or 86 => "🌨",   // Snow showers
-            95 => "🌩",          // Storm
-            96 or 99 => "⛈",   // Hailstorm (gradobicie)
-            _ => "👽"            // Unkown code
+            0 => "☀",                      // Sunny
+            1 => "🌤",                      // Mostly sunny
+            2 => "⛅",                     // Partial overcast
+            3 => "☁",                      // Overcast
+            45 or 48 => "🌫",               // Fog
+            51 or 53 or 55 => "🌦",         // Light rain
+            61 or 63 or 65 => "🌧",         // Rain
+            71 or 73 or 75 or 77 => "❄",   // Snow
+            80 or 81 or 82 => "☔",        // Showers (przelotne opady)
+            85 or 86 => "🌨",               // Snow showers
+            95 => "🌩",                     // Storm
+            96 or 99 => "⛈",               // Hailstorm (gradobicie)
+            _ => "👽"                      // Unkown code
         };
     }
-
 
     public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         => throw new NotImplementedException();
