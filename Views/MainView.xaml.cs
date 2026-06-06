1349dcf2a5ab6659c97d7a221737583a08198822
Marcin <piatekmarcin@proton.me>
2026-06-06 06:04:58 +0200

    Create code-behind for main-view

---
+++ Views/MainView.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows.Controls;
+
+namespace MeteoForecast.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+}
