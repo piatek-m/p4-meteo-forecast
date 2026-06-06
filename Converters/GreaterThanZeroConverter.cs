493c6ff487717f0aa7e37de4742f59a93e8a35ed
Marcin <piatekmarcin@proton.me>
2026-06-06 05:48:09 +0200

    Create GreaterThanZero converter

---
+++ Converters/GreaterThanZeroConverter.cs
@@ -0,0 +1,16 @@
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace MeteoForecast.Converters;
+
+public class GreaterThanZeroConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is double d && d > 0) return Visibility.Visible;
+        return Visibility.Collapsed;
+    }
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        => throw new NotImplementedException();
+}
